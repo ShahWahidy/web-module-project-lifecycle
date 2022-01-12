7e96ca650331c24b9d9968e3596fdcca7e1cbebb
Ahmadshah Wahidy <shah.wahidy@gmail.com>
2022-01-11 16:35:57 -0800

    completed module

--- src/App.js
+++ src/App.js
@@ -1,9 +1,64 @@
-import React from 'react';
+import React, { useDebugValue } from 'react';
+import axios from 'axios';
+import User from './components/User';
+import FollowersList from './components/FollowerList';
+
 
 class App extends React.Component {
+  state = {
+    currentUser : 'shahwahidy',
+    gitUser : {},
+    followers: [],
+  }
+  componentDidMount(){
+    axios.get(`https://api.github.com/users/${this.state.currentUser}`)
+    .then((res) => {
+      this.setState({
+        ...this.state,
+        gitUser: res.data
+      })
+    })
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    if(this.state.gitUser != prevState.gitUser){
+      axios.get(`https://api.github.com/users/${this.state.currentUser}/followers`)
+      .then((res) => {
+        this.setState({
+          ...this.state,
+          followers: res.data
+        })
+      })
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      ...this.state,
+      currentUser: e.target.value,
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('clicked')
+    axios.get(`https://api.github.com/users/${this.state.currentUser}`)
+    .then((res) => {
+      this.setState({
+        ...this.state,
+        gitUser: res.data
+      })
+    })
+  }
   render() {
     return(<div>
-      Github Card
+      <h1>GITHUB INFO</h1>
+      <form onSubmit={this.handleSubmit}>
+        <input placeholder='Github Handle' onChange={this.handleChange}/>
+        <button>Search</button>
+      </form>
+      <User user={this.state.gitUser}/>
+      <FollowersList followers={this.state.followers}/>
     </div>);
   }
 }
---
+++ src/components/Follower.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Follower extends React.Component{
+    render(){
+        const {follower} =  this.props
+        return(<div>
+            <img width='200px' src={follower.avatar_url}/>
+            <a target='_blank' href={follower.html_url}><p>{follower.login}</p></a>
+        </div>)
+    }
+}
+
+export default Follower;
